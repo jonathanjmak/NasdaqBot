87f49eefbf08fcc03c3686278c8c92d978744887
Jonathan Mak <JMak@Jonathans-MacBook-Pro.local>
2017-09-16 16:49:35 -0400

    fixed echo

--- index.js
+++ index.js
@@ -56,6 +56,25 @@ app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
 
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
+
 function sendGenericMessage(sender) {
     let messageData = {
 	    "attachment": {
